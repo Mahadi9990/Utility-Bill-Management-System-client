d6125bd16c550e636980a3423d96c048b724625d
Mahadi9990 <mdshafayat9999@gmail.com>
2025-11-19 14:21:47 +0600

    up 04

--- src/components/Extra_1.jsx
+++ src/components/Extra_1.jsx
@@ -41,12 +41,12 @@ export default function Extra_1() {
 
   return (
     <div className=" text-white py-16 px-6">
-      <h1 className={`text-4xl ${light ? "text-black":"text-white"} text-center my-5 font-bold`}>Extra-1</h1>
+      <h1 className={`text-4xl ${light ? "text-white":"text-black"} text-center my-5 font-bold`}>Extra-1</h1>
       <div className="max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-12">
         {features.map((f, i) => (
           <div key={i} className={`space-y-3 rounded-2xl shadow-2xl p-5 hover:scale-90 transition-all outline-1 outline-[#FF5A0A]`}>
             <div>{f.icon}</div>
-            <h3 className={`text-xl font-semibold ${light ? "text-black":"text-white"} `}>{f.title}</h3>
+            <h3 className={`text-xl font-semibold ${light ? "text-white":"text-black"} `}>{f.title}</h3>
             <p className="text-gray-400 text-sm leading-relaxed">{f.desc}</p>
           </div>
         ))}
